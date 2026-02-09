bfe7036977c16976a461145b0f86be50dc51f615
Ryan Bradshaw <bradshr@bc.edu>
2026-02-09 15:53:40 -0500

    After 2.7

--- WordGarden/ContentView.swift
+++ WordGarden/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var wordsGuessed = 0
     @State private var wordsMissed = 0
     private let wordsToGuess = ["SWIFT","DOG","CAT"]
@@ -20,6 +21,10 @@ struct ContentView: View {
     @State private var wordToGuess = ""
     @State private var revealedWord = ""
     @State private var lettersGuessed = ""
+    @State private var guessesRemaining = maximumGuesses
+    private static let maximumGuesses = 8
+    @State private var playAgainButtonLabel = "Another Word?"
+    
     
     var body: some View {
         VStack {
@@ -41,6 +46,8 @@ struct ContentView: View {
             Text(gameStatusMessage)
                 .font(.title)
                 .multilineTextAlignment(.center)
+                .frame(height: 80)
+                .minimumScaleFactor(0.5)
                 .padding()
             Text(revealedWord)
                 .font(.title)
@@ -73,16 +80,30 @@ struct ContentView: View {
                                 return
                             }
                             guessALetter()
+                            updateGamePlay()
                         }
                     Button("Guess a Letter"){
                         guessALetter()
+                        updateGamePlay()
                     }
                     .buttonStyle(.bordered)
                     .tint(.mint)
                     .disabled(guessedLetter.isEmpty)
                 }
             } else{
-                Button("Another Word?"){
+                Button(playAgainButtonLabel){
+                    if currentWordIndex == wordsToGuess.count{
+                        currentWordIndex = 0
+                        wordsGuessed = 0
+                        wordsMissed = 0
+                        playAgainButtonLabel = "Another Word?"
+                    }
+                    wordToGuess = wordsToGuess[currentWordIndex]
+                    revealedWord = "_" + String(repeating: " _", count: wordToGuess.count-1)
+                    lettersGuessed = ""
+                    guessesRemaining = Self.maximumGuesses
+                    imageName = "flower\(guessesRemaining)"
+                    gameStatusMessage = "How Many Guesses to Unconver the Hidden Word?"
                     playAgainHidden = true
                 }
                 
@@ -107,7 +128,32 @@ struct ContentView: View {
         revealedWord = wordToGuess.map{
             letter in lettersGuessed.contains(letter) ? "\(letter)" : "_"
         }.joined(separator: " ")
+    }
+    func updateGamePlay(){
+        
+        if !wordToGuess.contains(guessedLetter){
+            guessesRemaining -= 1
+            imageName = "flower\(guessesRemaining)"
+        }
+        if !revealedWord.contains("_"){
+            gameStatusMessage = "You Guessed It! It Took You \(lettersGuessed.count) Guesses to Guess the Word"
+            wordsGuessed += 1
+            currentWordIndex += 1
+            playAgainHidden = false
+        } else if guessesRemaining == 0{
+            gameStatusMessage = "So Sorry, You''re All Out of Guesses"
+            wordsMissed += 1
+            currentWordIndex += 1
+            playAgainHidden = false
+        } else{
+            gameStatusMessage = "You've Made \(lettersGuessed.count) Guess\(lettersGuessed.count == 1 ? "" : "es")"
+        }
+        if currentWordIndex == wordsToGuess.count{
+            playAgainButtonLabel = "Restart Game?"
+            gameStatusMessage = gameStatusMessage + "\nYou've Tried All The Words."
+        }
         guessedLetter = ""
+
     }
 }
 
