cc2eeeb4b6d959a360489f30921695edc483e58d
Ryan Bradshaw <bradshr@bc.edu>
2026-02-09 14:07:11 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Ryan Bradshaw on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
